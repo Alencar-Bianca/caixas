ce2c1ddb5083f8bae78131b53c6948dec634b1da
Alencar-Bianca <alencar-bianca@hotmail.com>
2021-05-25 21:09:14 -0300

    Update

--- js/slide.js
+++ js/slide.js
@@ -3,15 +3,15 @@ $(function(){
     
         function changeSlide(){
             let currentIndex = 0;
-            let maxIndex = $('.main-display .imgbanner').length;
+            let maxIndex = $('.imgbanner').length;
             let delay = 4000;
             setInterval(function(){
-            $('.main-display .imgbanner').eq(currentIndex).fadeOut(2000);
+            $('.imgbanner').eq(currentIndex).fadeOut(2000);
             currentIndex++;
             if(currentIndex == maxIndex){
                 currentIndex = 0;
             }
-            $('.main-display .imgbanner').eq(currentIndex).fadeIn(2000);
+            $('.imgbanner').eq(currentIndex).fadeIn(2000);
              },delay);
         }
     
